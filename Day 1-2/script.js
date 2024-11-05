a73025365eef36b7c9eec6fdd254513fefaabe97
Ritika027 <145582027+Ritika027@users.noreply.github.com>
2024-11-05 23:40:19 +0530

    Add files via upload

---
+++ Day 1-2/script.js
@@ -0,0 +1,37 @@
+//const = a variable that cant be changed
+const pi = 3.14; 
+//when let used furthur the value of pi can be changed
+//but if we use const it cant be changes
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+//converting radius into number
+
+circumference = 2*pi*radius;
+
+console.log(circumference);
+
+//MATH IS A BUILT-IN OBJECT THAT PROVIDES A COLLECTION OF PROPERTIES AND METHODS.
+let x=3.21;
+let y =2;
+let z;
+
+//z=Math.round(x);
+ //the answer will be 3
+
+ //z=Math.floor;
+ //this will also show 3 answer
+ //this means round off to nearest low number
+
+ //z=Math.ceil;
+ //answer will be 4
+ //this round off to nearest larger number
+
+
+ //z=Math.trunc(x);
+ //it will remove fraction part
+
+ //Math.pow , Math.sqrt,max.min,log,trigo funcn
+ 
